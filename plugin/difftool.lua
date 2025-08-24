1c04d8b68cfd6d34313db7cb6f6d02f389229f59
Tomas Slusny <slusnucky@gmail.com>
2025-08-24 18:37:42 +0200

    fix require
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- plugin/difftool.lua
+++ plugin/difftool.lua
@@ -5,7 +5,7 @@ vim.g.loaded_difftool = true
 
 vim.api.nvim_create_user_command('DiffTool', function(opts)
   if #opts.fargs == 2 then
-    require('nvim.difftool').diff(opts.fargs[1], opts.fargs[2])
+    require('difftool').diff(opts.fargs[1], opts.fargs[2])
   else
     vim.notify('Usage: DiffTool <left> <right>', vim.log.levels.ERROR)
   end
